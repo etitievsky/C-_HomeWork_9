caefa9b2caa816d9985ce5114eea867cb0c5d79d
etitievsky <etitievsky@mail.ru>
2023-01-13 15:02:51 +0300

    Task 02

---
+++ Task_02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SumNumbers(int a, int b)
+{
+    if(a>b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    if(a==b) return b;
+    return (a + SumNumbers(a+1, b));
+}
+
+int m = EnterData("Введите число M: ");
+int n = EnterData("Введите число N: ");
+Console.Write("Сумма чисел в промежутке от M до N = "+SumNumbers(m, n));
